277497769042b18a338cad200e30d9d5016b8be5
Oleksii <alekseystroy@ukr.net>
2024-04-20 22:50:05 +0300

    jshw7

--- jshw7.js
+++ jshw7.js
@@ -17,6 +17,35 @@
 // Метод isNaN (is Not-a-Number) в JavaScript використовується для перевірки, чи є передане значення нечисловим (NaN) типом.
 // Цей метод повертає true, якщо значення є NaN, і false, якщо значення може бути конвертовано в число.
 
+function myIsNaN(value) {
+  return Number(value) !== Number(value);
+}
+
+console.log(myIsNaN(NaN)); // true
+console.log(myIsNaN(undefined)); // true
+console.log(myIsNaN({})); // true
+
+console.log(myIsNaN(true)); // false
+console.log(myIsNaN(null)); // false
+console.log(myIsNaN(37)); // false
+
+// Strings
+console.log(myIsNaN("37")); // false
+console.log(myIsNaN("37.37")); // false
+console.log(myIsNaN("37,5")); // true
+console.log(myIsNaN("123ABC")); // true
+console.log(myIsNaN("")); // false
+console.log(myIsNaN(" ")); // false
+
+// Dates
+console.log(myIsNaN(new Date())); // false
+console.log(myIsNaN(new Date().toString())); // true
+
+// Arrays
+console.log(myIsNaN([])); // false
+console.log(myIsNaN([1])); // false
+console.log(myIsNaN([1, 2])); // true
+
 
 
 
@@ -42,6 +71,30 @@
 // Створіть гілку js_homework4.
 // Додати туди ваше домашне завдання
 // Вислати вашу гілку
+
+
+// //наче так
+function pad(string, symbolPlus, numSym, boolSym) {
+  const symbol = symbolPlus.repeat(1);
+  const diff = numSym - string.length;
+  if (diff <= 0 || numSym <= string.length) {
+      return string;
+  }
+  const repeatedSymbol = symbol.repeat(diff);
+  if (boolSym) {
+      return repeatedSymbol + string;
+  } else {
+      return string + repeatedSymbol;
+  }
+}
+
+console.log(pad('qwerty', '+', 6, true));
+console.log(pad('kek', '+', 8, false));
+
+
+
+
+
 // ﻿ДЗ3
 
 // Перевірка теорії ймовірності. Напишіть функцію яка буде генерувати певну кількість випадкових чисел в діапазоні від 100 до 1000 включно. Порахувати кількість парних та непарних серед них. Обчислити відсоткове співвідношення - чи буде воно близьке до 50%50? Приклад функції checkProbabilityTheory(count). Парметр count буде вказувати скільки разів буде генеруватися випадкове число.
@@ -52,12 +105,27 @@
 // Кількість згенерованих чисел: кількість чисел
 // Парних чисел: кількість парних чисел
 // Не парних чисел: кількість не парних чисел
-// Відсоток парних до не парних: 
-// Не використовувати функції або інші матеріали які ми не вивчали
-// Намагайтесь робити ДЗ самостійно та не дивитися будьякі підказки
-// Будь ласка, надайте наступні дані для перевірки вашої роботи:
-
-// Створіть гілку js_homework4.
-// Додати туди ваше домашне завдання
+// Відсоток парних до не парних:
 
 
+function checkProbabilityTheory(count) {
+  let evenCount = 0;
+  let oddCount = 0;
+
+  for (let i = 0; i < count; i++) {
+      const randomNumber = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+      if (randomNumber % 2 === 0) {
+          evenCount++;
+      } else {
+          oddCount++;
+      }
+  }
+  const percentageEven = (evenCount / count) * 100;
+  const percentageOdd = (oddCount / count) * 100;
+
+  console.log("Кількість згенерованих чисел:", count);
+  console.log("Парних чисел:", evenCount);
+  console.log("Непарних чисел:", oddCount);
+  console.log("Відсоток парних до не парних:", percentageEven.toFixed(2) + "% : " + percentageOdd.toFixed(2) + "%");
+}
+checkProbabilityTheory(2);
