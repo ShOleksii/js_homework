10d8bdea6bca30c096f204b34a32f55e722e073d
Oleksii <alekseystroy@ukr.net>
2024-04-20 19:31:44 +0300

    jshw10-2

--- jshw10.js
+++ jshw10.js
@@ -7,14 +7,37 @@
 
 // Дз2
 
+// У нас параноя, ми не довіряємо жодним адресам, окрім зареєстрованих на доменах gmail.com та yahoo.com.
+// За допомогою регулярного виразу створіть масив з адресами, гідними довіри. Постарайтеся також зробити просту валідацію до @
+// - одне, або два слова які можуть містити (латинські букви, цифри) яке може бути розділене крапкою.
 
+let arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
 
+let validEmailRegex = /^[a-zA-Z0-9.-_]+@(gmail\.com|yahoo\.com)$/;
 
-
-
-
-
-
+let trustedEmails = arr.filter(function(obj) {
+  return validEmailRegex.test(obj.email);
+});
+console.log("Email-адреси:");
+for (let item of trustedEmails) {
+    console.log(item.email);
+}
 
 
 
